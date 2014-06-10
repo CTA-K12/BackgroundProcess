9c6d4b0eb740728c380291494edc383a9a5aef5c
lighthart <dlighthart@mesd.k12.or.us>
2014-06-10 13:08:03 -0700

    Rebuilt bundle class

---
+++ MesdBgProcessBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mesd\BgProcessBundle;
+
+use Symfony\Component\Console\Application;
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MesdBgProcessBundle extends Bundle
+{
+
+    public function registerCommands(Application $application){
+        //$crudCommand = $application->get('generate:doctrine:crud');
+        //$generator = new DoctrineCrudGenerator(new FileSystem, __DIR__.'/Resources/skeleton/crud');
+        //$crudCommand->setGenerator($generator);
+
+        parent::registerCommands($application);
+    }
+
+}
